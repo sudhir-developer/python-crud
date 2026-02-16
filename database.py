30afa83f27b835f4c41b520bbce6c4c457346b35
sudhir-developer <sudhirkumarpatel1810@gmail.com>
2026-02-16 12:40:58 +0530

    dabase.py fixes

--- database.py
+++ database.py
@@ -1,7 +1,11 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://mac@localhost/wyzard_db"
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://mac@localhost/wyzard_db"
+)
 
 engine = create_engine(DATABASE_URL)
 
